da058dc31cdaa876d5f0ae5d0727acf4948fd3fe
kcortes133 <kcortes@ucsd.edu>
2018-06-05 08:15:05 -0700

    Update pfam.py

--- pfam.py
+++ pfam.py
@@ -42,8 +42,42 @@ go_terms
 category
 term
 go_id ="" 
+
+
+1. Get id by searching embl-cds 
+ - 
+2. Run BLAST against UNIPROT on that id
+ - https://www.uniprot.org/blast/uniprot/B2018060583C3DD8CE55183C76102DC5D3A26728B1D87CDE.xml
+
+
+EBIApplicationResult
+SequenceSimilaritySearchResult
+hit
+hit
+descrtion
+
 """
 
+# Getting the identifier
+"""
+import urllib,urllib2
+
+url = 'http://www.uniprot.org/uploadlists/'
+
+params = {
+'from':'ACC',
+'to':'P_REFSEQ_AC',
+'format':'tab',
+'query':'P13368 P20806 Q9UM73 P97793 Q17192'
+}
+
+data = urllib.urlencode(params)
+request = urllib2.Request(url, data)
+contact = "" # Please set a contact email address here to help us debug in case of problems (see https://www.uniprot.org/help/privacy).
+request.add_header('User-Agent', 'Python %s' % contact)
+response = urllib2.urlopen(request)
+page = response.read(200000)
+"""
 
 basic_url = "https://www.uniprot.org/uniprot/"
 uid = "P12345"
