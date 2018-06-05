23b4dd6be03a0255abafab73ad750bbad82fc169
Uma Mahto <umahto@ucsd.edu>
2018-06-05 15:29:04 -0700

    Update GO.py

--- GO.py
+++ GO.py
@@ -15,30 +15,6 @@ To download the GO OBO file, we need the wget and os libraries.
 import wget
 import os
 
-def transitive_closure(go_term, go):
-    go_term_set = set()
-    find_parents(go_term, go, go_term_set)
-    find_children(go_term, go, go_term_set)
-    return go_term_set
-    
-def find_parents(term1, go, go_term_set={}, ret=False):
-    for term2 in term1.parents:
-        go_term_set.update({term2})
-        
-        # Recurse on term to find all parents
-        find_parents(term2, go, go_term_set)          
-    if(ret):
-        return go_term_set
-
-def find_children(term1, go, go_term_set={}, ret=False):
-    for term2 in term1.children:
-        go_term_set.update({term2})
-        
-        # Recurse on term to find all children
-        find_children(term2, go, go_term_set)
-    if(ret):
-        return go_term_set
-
 go_obo_url = 'http://purl.obolibrary.org/obo/go.obo'
 data_folder = os.getcwd() + '/data'
 
@@ -62,9 +38,20 @@ else:
 '''create a dictionary of the GO terms - using the obo_parser from GOATools'''
 go = obo_parser.GODag(go_obo)
 
-growth_count = 0
-for go_term in go.values():
-    if 'pterin' in go_term.name:
-        growth_count += 1
+'''create a dictionary of accession numbers and go terms queried from prosite/pfam/uniprot'''
+accgoid = {}
 
-print('Number of GO terms with "growth" in their name: {}'.format(growth_count))
+'''create a dictionary of go id and go term names'''
+goid = {}
+
+'''temp file name is querygo.txt'''
+with open("querygo.txt") as f:
+    for line in f:
+        (key, val) = line.split()
+        accgoid[key] = val
+        '''for each go id search the GO db'''
+        for val in go.values():
+            goid[val] = go_term.name
+            
+print(accgoid)
+print(goid)
