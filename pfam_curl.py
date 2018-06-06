6df7b5455eb7fca0e8337a0e3120365c89d04399
kcortes133 <kcortes@ucsd.edu>
2018-06-05 17:38:24 -0700

    changes

--- pfam_curl.py
+++ pfam_curl.py
@@ -4,18 +4,16 @@ import os, subprocess, xmltodict, json, csv, time, ast
 
 
 def get_results(url):
-    command = "curl -LH 'Expect:' 'https://pfam.xfam.org" + url 
+    command = "curl -LH 'Expect:' 'https://pfam.xfam.org" + url +"'"
     print("command : ", command)
     output = os.popen(command).read()
     result = json.dumps(xmltodict.parse(output))
-    print(result)
-    info = []
+    info = {}
     temp_result = result['pfam']['results']['matches']['protein']['database']['match']
-    info.append(temp_result['accession'])
-    info.append(temp_result['type'])
-    info.append(temp_result['class'])
-    return info
-
+    info['accession'] = temp_result['@accession']
+    info['type'] = temp_result['@type']
+    info['class'] = temp_result['@class']
+    return temp_result
 
 def search(seq):
     with open("out.txt", 'w') as outf:
@@ -33,20 +31,25 @@ with open('UP000006737.fasta', 'r') as inf:
 cont = False
 temp = ""
 j = 0
-with open("pfam_info", 'w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=",")
+with open("pfam_info", 'w') as outfile:
     for l in lines:
         if '>' in l:
             j+=1
             if j>1:
                 r_url = search(temp)
-                time.sleep(180)
-                info = get_results(r_url)
-                writer.writerow(info)
-                print(info)
+                time.sleep(200)
+                try:
+                    info = get_results(r_url)
+                    protien = {str(j) : info}
+                    outfile.write(json.dumps(info))
+                    print(info)
+                except:
+                    protien = {str(j): 'None'}
                 temp = ""
         else:
             temp = temp + l
+        if j > 120:
+            break
 
 
     
